bd4bb340053c934b2fd57f32b56eea4f62eacc13
Ianthe van Belzen <ivanbelzen@hpcs03.op.umcutrecht.nl>
2021-12-30 09:50:10 +0100

    run script

--- prepare_run_singularity.sh
+++ prepare_run_singularity.sh
@@ -3,6 +3,7 @@
 ## Run fusion sq
 
 test_identifier=$1
+test_identifier="wilms_v2_20210923"
 singularity_img="/hpc/pmc_gen/ivanbelzen/structuralvariation/structural_variation_latest.sif"
 
 fusionsq_dir="/hpc/pmc_gen/ivanbelzen/github_fusion_sq/fusion-sq/R/"
@@ -15,7 +16,7 @@ cohort_file="${cohort_dir}cohort.${test_identifier}.tsv"
 cohort_run_file=${cohort_file}
 
 #run subset or full cohort
-cohort_run_file="${cohort_dir}run_cohort.wilms_v2_20211002.tsv"
+cohort_run_file="${cohort_dir}run_cohort.wilms_v2_20211020.tsv"
 config_general="${cohort_dir}${test_identifier}.conf"
 
 output_singularity_dir="${cohort_dir}output/"
@@ -27,13 +28,15 @@ gridss_dir="${source_dir}data/gridss-2.7.2/blklst_dac_decoy/"
 manta_dir="${source_dir}data/manta-1.6.0/"
 delly_dir="${source_dir}data/delly-0.8.1/blklst_dac_decoy/"
 archive_dir="${source_dir}structuralvariation/v1/${test_identifier}/"
+
+#star_fusion_dir=...
 #final_output_file_ext=".somatic.svs_merged.tsv"
 
 TMPDIR="${input_vcf_data_dir}"
 
 prepare_script="${fusionsq_dir}prepare_matching_intervals.docker.R"
 match_wgs_script="${fusionsq_dir}run_match_wgs.docker.R"
-
+combine_wgs_script="${fusionsq_dir}combine_wgs_support.docker.R"
 
 while read -r patient_id tumor_id normal_id rna_id <&3; do
       if [[ ${patient_id} == "patient_id" ]];  then continue; fi
@@ -51,12 +54,16 @@ while read -r patient_id tumor_id normal_id rna_id <&3; do
 
     echo "#!/usr/bin/env bash" > ${runscript}
     echo "export TMPDIR=${TMPDIR}" >> ${runscript}
+
+#rsync -avhe ssh gerrit:/data/groups/gen/ivanbelzen/data/starfusion/wilms_v2/PMLBM000CVQ*RNA-Seq.star-fusion_predicted.tsv /hpc/pmc_gen/ivanbelzen/structuralvariation/tmp/.
     
     echo "SINGULARITYENV_R_MAX_VSIZE=100Gb" >> ${runscript}
     echo "singularity exec --bind /hpc/pmc_gen/ivanbelzen/ ${singularity_img} R -e \"${singularity_settings}source('${prepare_script}')\"" >> ${runscript}
     echo "singularity exec --bind /hpc/pmc_gen/ivanbelzen/ ${singularity_img} R -e \"${singularity_settings}run_tool='manta';source(paste0(script_dir,'functions.get_vcf_filepath.docker.R'));source('${match_wgs_script}')\"" >> ${runscript}
     echo "singularity exec --bind /hpc/pmc_gen/ivanbelzen/ ${singularity_img} R -e \"${singularity_settings}run_tool='delly';source(paste0(script_dir,'functions.get_vcf_filepath.docker.R'));source('${match_wgs_script}')\"" >> ${runscript}
     echo "singularity exec --bind /hpc/pmc_gen/ivanbelzen/ ${singularity_img} R -e \"${singularity_settings}run_tool='gridss';source(paste0(script_dir,'functions.get_vcf_filepath.docker.R'));source('${match_wgs_script}')\"" >> ${runscript}
+
+    echo "singularity exec --bind /hpc/pmc_gen/ivanbelzen/ ${singularity_img} R -e \"${singularity_settings}source('${combine_wgs_script}')\"" >> ${runscript}
 
     echo "#rsync -avhe ssh ${output_singularity_dir}${tumor_id}_${normal_id}.* ${host_server}:${archive_dir}" >> ${runscript}
     
